ff3d8cc13946cc64e0b0505f54ba9e9d017db09b
Kai DiRamio <kaidiramio@gmail.com>
2022-04-25 11:45:02 -0400

    Daily CodeWar

---
+++ CatYearsDogYears.js
@@ -0,0 +1,47 @@
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+const dogCatAge = (humanYears) => {
+    // return humanYears, catYears, dogYears
+    let catYears = 0
+    let dogYears = 0
+
+    // if humanYears is x catYears is y 
+    // if humanYears is x dogYears is y
+    // conditionals for age
+    
+    for (let i = 1; i < humanYears.length; i++){
+        if (humanYears === 1){
+            catYears = 15
+            dogYears = 15
+        }else if (humanYears === 2){
+        catYears = 24
+        dogYears = 24
+        }else if (humanYears >= 3){
+            catYears += 4
+            dogYears += 5
+        }
+    }
+    return (humanYears, dogYears, catYears)
+}
+
+dogCatAge(6)
+
