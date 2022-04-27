5047b05106720c9059d81fbf8a675ab89b43b488
Kai DiRamio <kaidiramio@gmail.com>
2022-04-26 21:22:21 -0400

    nameShuffler CodeWar

---
+++ nameShuffler.js
@@ -0,0 +1,7 @@
+function nameShuffler(str){
+    return str.split(' ').reverse().join(" ");
+  }
+  
+  // str divides a String into an ordered list of substrings
+  // reverses and then joins/ concats elements
+  
