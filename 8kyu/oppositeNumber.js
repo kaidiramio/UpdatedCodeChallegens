d7caad094197a54664e304dbb4d22cc9dde8bd0e
Kai DiRamio <kaidiramio@gmail.com>
2022-05-05 23:25:17 -0400

    05/05

---
+++ 8kyu/oppositeNumber.js
@@ -0,0 +1,15 @@
+// Opposite number
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+    if (number < 0){
+      return number*-1;
+     
+    //- * - = positive and - * + = negative
+    }else{
+      return -number
+    }
+  }
+
+  
+
