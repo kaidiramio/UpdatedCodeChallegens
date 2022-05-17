e5c89e53bdbcbbc5b575abef2ad34705e40ffd6e
Kai DiRamio <kaidiramio@gmail.com>
2022-05-16 20:20:15 -0400

    Codewar

---
+++ 8kyu/ReturningStrings.js
@@ -0,0 +1,8 @@
+// Returning Strings
+
+function greet(name){
+    //   return a greeting statement that uses an input
+      return `Hello, ${name} how are you doing today?`
+}
+    
+// we can use a template literal string
