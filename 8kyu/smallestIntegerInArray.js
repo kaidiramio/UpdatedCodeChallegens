f181d9bdd5f958cb6038cc2f3e2901821b3b5833
Kai DiRamio <kaidiramio@gmail.com>
2022-05-17 14:29:17 -0400

    codewar

---
+++ 8kyu/smallestIntegerInArray.js
@@ -0,0 +1,10 @@
+// codewar 05/17 - Find the smallest integer in the array
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+  //     return using Math.min() method (pass values (args) through method)
+      return Math.min(...args)
+      
+    //returns the smallest of the given numbers
+    }
+}
