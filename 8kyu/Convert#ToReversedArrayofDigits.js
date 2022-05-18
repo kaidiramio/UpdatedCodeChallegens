ced8a4aeb4247ae66fce36ecc2ed2d444eea11d5
Kai DiRamio <kaidiramio@gmail.com>
2022-05-18 16:14:17 -0400

    8kyu

---
+++ 8kyu/Convert#ToReversedArrayofDigits.js
@@ -0,0 +1,12 @@
+// Convert number to reversed array of digits
+
+const digitize= (n) => n.toString().split('').map(Number).reverse();
+
+// get number
+// number(s) into array
+// number(s) into a string
+// reverese elements in array
+
+// .reverse takes a list and reverses the elements
+// declare function digitize
+// map each element and turn it back into a number
