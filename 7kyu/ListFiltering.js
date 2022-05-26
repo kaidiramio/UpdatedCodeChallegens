ef77f956720f85dac33d5193d6b9aadec3362f9f
Kai DiRamio <kaidiramio@gmail.com>
2022-05-25 22:27:45 -0400

    codewar

---
+++ 7kyu/ListFiltering.js
@@ -0,0 +1,16 @@
+// List Filtering 7KYU
+
+function filter_list(l) {
+    return l.filter(Number.isInteger);
+    // parameter filtered for number - new array is returned with strings filtered.
+}
+
+// filter for string 
+// return new array
+
+// 1. The filter() method creates a new array with all elements that pass the test implemented by the provided function. 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+// 2. The Number.isInteger() method determines whether the passed value is an integer.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+
