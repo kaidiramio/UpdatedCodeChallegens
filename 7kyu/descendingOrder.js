72cc81814658a0d01f92ba82b84cbedb438bb695
Kai DiRamio <kaidiramio@gmail.com>
2022-05-30 00:11:32 -0400

    codewar

---
+++ 7kyu/descendingOrder.js
@@ -0,0 +1,12 @@
+// descending Order
+
+function descendingOrder(n){
+    return parseInt((n+'').split('').sort().reverse().join(''))
+}
+
+// parseInt returns a string 
+// split splits into array
+// reverse reverse array
+// join with join them together like 1234 (no space)
+
+// going over this again in more depth
