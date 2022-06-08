d12cf20c3fde2dde160287697d7b5385ac9e6f14
Kai DiRamio <kaidiramio@gmail.com>
2022-06-07 22:50:03 -0400

    updated

--- 8kyu/booleanToString.js
+++ 8kyu/booleanToString.js
@@ -1,9 +1,26 @@
+// DESCRIPTION:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+// params b, takes boolean value and returns yes string for true ? no string for false
+// return a boolean  
+// example okay => OKAY
+// 
+
+
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
+}
+
+// OR 
+
 function booleanToString(b){
-    if(b){
-      return 'true';
-    }else{
-      return 'false';
-    }
+  if(b){
+    return 'true';
+  }else{
+    return 'false';
   }
-  
+}
+
   // takes parameter and sets a conditional that returns a string 
