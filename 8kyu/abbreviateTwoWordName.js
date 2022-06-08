42651700a966ecc506b1b8ac8473482385ac996d
Kai DiRamio <kaidiramio@gmail.com>
2022-06-07 23:08:05 -0400

    up

--- 8kyu/abbreviateTwoWordName.js
+++
@@ -1,9 +0,0 @@
-function abbrevName(name){
-    const answer = [name[0]]
-    for(let i = 0; i < name.length; i++) {
-        if(name[i] === " ") {
-            answer.push(name[i + 1]);
-        }
-    }
-    return answer.map(el => el.toUpperCase()).join(".")
-}
