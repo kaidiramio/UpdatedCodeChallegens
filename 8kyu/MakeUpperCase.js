cf5d5ced57e0ac856c936f3f39850cd85551f993
Kai DiRamio <kaidiramio@gmail.com>
2022-06-07 22:39:56 -0400

    codewar

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,17 @@
+// MakeUpperCase - https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/solutions/javascript
+
+// Write a function which converts the input string to uppercase.
+
+
+
+// params str
+// return a string in uppercase 
+// example okay => OKAY
+// 
+
+function makeUpperCase(str) {
+    //   use toUpperCase method to convert string to uppercase
+      let newString = str.toUpperCase()
+      
+      return newString
+    }
