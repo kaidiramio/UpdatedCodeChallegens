faf379eb39ec4154ea03958d1e569c905bd4caba
Kai DiRamio <kaidiramio@gmail.com>
2022-06-12 23:06:16 -0400

    codewar

---
+++ 8kyu/AreaOrPerimeter.js
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab/javascript
+
+// Area or Perimeter
+
+// DESCRIPTION:
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+
+const areaOrPerimeter = function(l , w) {
+    //   length // width 
+    //   if square [EQUAL] return area == (l*w)
+    //   if rectangle return perimeter == (l+w)*2
+    //   example: 6, 10 --> 32 ; rectangle
+    //   3, 3, --> 9 ; square
+    if(l === w){
+        return l * w
+    }else {
+        return (l + w) * 2
+    }
+      
+};
