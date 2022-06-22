eea7aa0c649199bed93b315358817607f648fd94
Kai DiRamio <kaidiramio@gmail.com>
2022-06-21 22:54:12 -0400

    codewar

---
+++ 8kyu/ConvertStringToArray.js
@@ -0,0 +1,20 @@
+// Convert a string to an array => https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/javascript
+
+// DESCRIPTION:
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+function stringToArray(string){
+
+	let strArray = string.split(' ')
+//   ('') = split characters in words 
+//   (' ') = split words in string
+  
+  return strArray
+
+} 
