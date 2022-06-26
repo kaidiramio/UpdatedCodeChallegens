c18fdc01ee7dee6a2ae6a15ab2f8b9b64f3ef85b
Kai DiRamio <kaidiramio@gmail.com>
2022-06-26 10:45:19 -0400

    update

---
+++ 8kyu/FunctionMultiplyTwoNumbers.js
@@ -0,0 +1,10 @@
+// Function 3 - multiplying two numbers
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/javascript
+
+// DESCRIPTION:
+// Implement a function that multiplies 2 numbers 
+
+
+function multiply(num1, num2){
+    return  num1 * num2;
+}
