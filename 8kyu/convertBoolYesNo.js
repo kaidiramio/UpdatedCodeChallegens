729c253ef951ce240e0a091bda6d4522bb3f61ef
Kai DiRamio <kaidiramio@gmail.com>
2022-07-11 16:59:14 -0400

    conditionals instead

--- 8kyu/convertBoolYesNo.js
+++ 8kyu/convertBoolYesNo.js
@@ -1,5 +1,26 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+// DESCRIPTION:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// conditional 
+
+function boolToWord(bool){
+
+  // pass parameter => conditional if boolean =
+  if (bool) {
+    // if bool return 'Yes'
+    return 'Yes';
+     // if NOT bool return 'No'
+  } else {
+    return 'No';
+  }
+}
+
+// OR 
+
 function boolToWord( bool ){
     return bool ? 'Yes':'No';
-  }
+}
 
   // ternary syntax condition ? exprIfTrue : exprIfFalse 
