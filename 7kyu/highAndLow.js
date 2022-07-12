be6d46cb60b3328296c56ad5a1b18bc35b54b5fd
Kai DiRamio <kaidiramio@gmail.com>
2022-07-12 12:46:56 -0400

    updated w/link and info

--- 7kyu/highAndLow.js
+++ 7kyu/highAndLow.js
@@ -1,12 +1,53 @@
+// Highest and Lowest - https://www.codewars.com/kata/554b4ac871d6813a03000035/javascript
+
+// DESCRIPTION:
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+// More efficient using math max and math min methods
+
 function highAndLow(numbers){
-    //   create variables
-      var highLow = numbers.split(" ");
-      let maxNum = highLow[0]
-      let minNum = highLow[0]
+    //   create variables // param numbers = string that we will split into an array
+      const highLowArr = numbers.split(' ');
+          console.log (numbers.split(' '))
+          
+    // variables for max and min number taken from highLowArray using math.max method etc
+      let maxNum = Math.max(...highLowArr)
+//       console.log(maxNum)
       
-    //   loop through numbers using variables above (max and min num)
+      let minNum =  Math.min(...highLowArr)
     
-        for(let num of highLow) {
+      console.log(maxNum + ' ' + minNum)
+    //   return the maxNum, space, minNum
+}
+
+highAndLow('1 2 3 4 5')
+
+// or another way
+
+function highAndLow(numbers){
+    //   create variables // param numbers = string that we will split into an array
+      var highLowArr = numbers.split(" ");
+          console.log (numbers.split(' '))
+          
+//        variables for max and min number taken from the highlow variable
+      let maxNum = highLowArr[0]
+      console.log(maxNum)
+      
+      let minNum = highLowArr[0]
+      
+    //  for of loop through the array of numbers using variables above (max and min num)
+    
+        for(let num of highLowArr) {
             if(Number(num) > maxNum) {
                 maxNum = num
     //           if number is greater than maxNum, maxNum equals num
@@ -18,6 +59,8 @@ function highAndLow(numbers){
           }
         }
     
-        return maxNum + " " + minNum
+        return(maxNum + " " + minNum)
     //   return the maxNum, space, minNum from loop above
 }
+highAndLow("1 2 3 4 5")
+
